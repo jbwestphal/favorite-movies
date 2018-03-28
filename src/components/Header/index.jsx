0a289d285a0963663733cabb887be1ce414e538c
Jessica Westphal <jbwestphal85@gmail.com>
2018-03-28 17:00:59 -0300

    feat: Add layout header and cover body

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import styled from "styled-components";
 
 import Navigation from '../Navigation';
 import Search from '../Search';
 import logo from './logo.png';
 
 const Header = (props) =>
-  <header className="header">
+  <StyledHeader className="header">
     <h1 className="header-logo"><img src={logo} alt="Favorite Movies" /></h1>
     <Navigation />
     <Search />
-  </header>
+  </StyledHeader>
+
+// Styles
+const StyledHeader = styled.header`
+  top: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  position: fixed;
+  letter-spacing: 1px;
+`;
 
 export default Header;
--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
-import FontAwesome from 'react-fontawesome';
+// import FontAwesome from 'react-fontawesome';
 import styled from "styled-components";
 
 import SignOutButton from '../SignOut';
 import * as routes from '../../constants/routes';
 
 const Navigation = (props, { authUser }) =>
-  <nav className="header-nav">
+  <StyledNav className="header-nav">
     <NavList className="header-nav-list">
-      <NavListItem><Link to={routes.LANDING}><FontAwesome name='home' /> Home</Link></NavListItem>
-      <NavListItem><Link to={routes.LANDING}><FontAwesome name='film' /> Movies</Link></NavListItem>
-      <NavListItem><Link to={routes.LANDING}><FontAwesome name='question-circle' /> FAQ</Link></NavListItem>
-      <NavListItem><Link to={routes.LANDING}><FontAwesome name='headphones' /> Contact</Link></NavListItem>
+      <NavListItem><Link to={routes.LANDING}>Home</Link></NavListItem>
+      <NavListItem><Link to={routes.LANDING}>Movies</Link></NavListItem>
+      <NavListItem><Link to={routes.LANDING}>FAQ</Link></NavListItem>
+      <NavListItem><Link to={routes.LANDING}>Contact</Link></NavListItem>
     </NavList>
 
     { authUser
         ? <NavigationAuth />
         : <NavigationNonAuth />
     }
-  </nav>
+  </StyledNav>
 
 Navigation.contextTypes = {
   authUser: PropTypes.object,
@@ -36,24 +36,28 @@ const NavigationAuth = () =>
 const NavigationNonAuth = () =>
   <ul className="header-login header-unlogged">
     <li className="header-nav-login"><Link className="btn btn-sm" to={routes.SIGN_IN}>Sign In</Link></li>
-    <li className="header-nav-register"><Link className="btn btn-sm" to={routes.SIGN_IN}>Register</Link></li>
+    <li className="header-nav-register"><Link className="btn btn-sm" to={routes.SIGN_UP}>Register</Link></li>
   </ul>
 
 
 // Styles
+const StyledNav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  & li {
+    display: inline-block;
+  }
+`;
+
 const NavList = styled.ul`
   font-size: 16px;
   text-transform: uppercase;
 `;
 
 const NavListItem = styled.li`
-  display: inline-block;
   margin: 0 25px;
-
-  & .fa {
-    font-size: 24px;
-    margin-right: 10px;
-  }
 `;
 
 export default Navigation;
