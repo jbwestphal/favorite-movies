9c93424ed7928446a7029a91569340b1622b8882
Jessica Westphal <jbwestphal85@gmail.com>
2018-04-14 20:38:03 -0300

    refactor: Contact form

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,39 +1,21 @@
 import React from 'react';
-
 import { StyledForm } from '../../utils/cssForm';
 
 class ContactForm extends React.Component {
-
-  onSubmit = (event) => {
+  onSubmit = event => {
     event.preventDefault();
-  }
+  };
 
   render() {
-
     return (
       <StyledForm>
         <p>If you have doubts, fill the form below and talk to us.</p>
         <form onSubmit={this.onSubmit}>
-          <input
-            type="text"
-            required
-            placeholder="Your Name"
-          />
-          <input
-            type="email"
-            required
-            placeholder="Your Email"
-          />
-          <input
-            type="text"
-            required
-            placeholder="Subject"
-          />
-          <input
-            type="text"
-            placeholder="Your Phone"
-          />
-          <textarea placeholder="Your Message"></textarea>
+          <input type="text" required placeholder="Your Name" />
+          <input type="email" required placeholder="Your Email" />
+          <input type="text" required placeholder="Subject" />
+          <input type="text" placeholder="Your Phone" />
+          <textarea placeholder="Your Message" />
           <button type="submit">Send</button>
 
           {/* { error && <p>{error.message}</p> } */}
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-import { TitlePage } from "../utils/cssTypo";
-import { Wrapper, PageContainer } from "../utils/cssGrid";
-import ContactForm from "../components/ContactForm";
+import { TitlePage } from '../utils/cssTypo';
+import { Wrapper, PageContainer } from '../utils/cssGrid';
+import ContactForm from '../components/ContactForm';
 
-const ContactPage = () =>
+const ContactPage = () => (
   <Wrapper>
     <PageContainer>
       <TitlePage>Contact us</TitlePage>
       <ContactForm />
     </PageContainer>
   </Wrapper>
+);
 
 export default ContactPage;
